e3c083969c326bf642f8dadfb369a56ca513bdc8
Artem Zaborshikov <a1az1z@ya.ru>
2021-11-27 20:21:09 +0300

    Add main file

---
+++ main.py
@@ -0,0 +1,33 @@
+#classes
+class FaceRegError(Exception):
+    None
+
+
+class Face():
+
+    def __init__(self, img):
+        '''
+        Создание лица. На вход подаётся массив numpy или название файла
+        '''
+        if type(img)==str:
+            img = self.file2img(img)
+        try:
+            self.face = face_recognition.face_encodings(img)[0]
+        except:
+            raise FaceRegError("Face registration error. The image is damaged or there is no face on it")
+
+    def check(self, unface:np.array):
+        '''
+        Сравнение лица с другим. На вход подаётся другое лицо в формате массива
+        '''
+        result = face_recognition.compare_faces([self.face], unface)
+        return result[0]
+
+    def file2img(self, file, mode='RGB'):
+        '''
+        Приводит файл к формату массива
+        '''
+        im = Image.open(file)
+        if mode:
+            im = im.convert(mode)
+        return np.array(im)
